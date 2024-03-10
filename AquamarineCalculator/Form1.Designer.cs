241e3babc152f671b3797b5b24dd83ea2427418c
MegamerStudios <cozoral@outlook.com>
2024-03-10 15:38:11 +0300

    Add project files.

---
+++ AquamarineCalculator/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace AquamarineCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            groupBox1 = new GroupBox();
+            button5 = new Button();
+            richTextBox3 = new RichTextBox();
+            groupBox2 = new GroupBox();
+            groupBox4 = new GroupBox();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            groupBox3 = new GroupBox();
+            richTextBox2 = new RichTextBox();
+            richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            label3 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(richTextBox3);
+            groupBox1.ForeColor = SystemColors.Control;
+            groupBox1.Location = new Point(12, 21);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(314, 124);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Output";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // button5
+            // 
+            button5.ForeColor = SystemColors.ActiveCaptionText;
+            button5.Location = new Point(8, 89);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 4;
+            button5.Text = "Copy";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            richTextBox3.Location = new Point(6, 22);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.ReadOnly = true;
+            richTextBox3.Size = new Size(302, 61);
+            richTextBox3.TabIndex = 2;
+            richTextBox3.Text = "";
+            richTextBox3.TextChanged += richTextBox3_TextChanged;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(groupBox4);
+            groupBox2.Controls.Add(groupBox3);
+            groupBox2.ForeColor = SystemColors.Control;
+            groupBox2.Location = new Point(12, 151);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(314, 266);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Input";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(button4);
+            groupBox4.Controls.Add(button3);
+            groupBox4.Controls.Add(button2);
+            groupBox4.Controls.Add(button1);
+            groupBox4.ForeColor = SystemColors.Control;
+            groupBox4.Location = new Point(8, 95);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(300, 165);
+            groupBox4.TabIndex = 4;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Operations";
+            // 
+            // button4
+            // 
+            button4.ForeColor = SystemColors.ActiveCaptionText;
+            button4.Location = new Point(15, 51);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 3;
+            button4.Text = "Divide";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(177, 22);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Multiply";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(96, 22);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Subtract";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(15, 22);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(richTextBox2);
+            groupBox3.Controls.Add(richTextBox1);
+            groupBox3.ForeColor = SystemColors.Control;
+            groupBox3.Location = new Point(8, 22);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(300, 67);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Numbers";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(150, 22);
+            richTextBox2.Multiline = false;
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(144, 37);
+            richTextBox2.TabIndex = 1;
+            richTextBox2.Text = "";
+            richTextBox2.TextChanged += richTextBox2_TextChanged;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(6, 22);
+            richTextBox1.Multiline = false;
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(138, 37);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(12, 420);
+            label1.Name = "label1";
+            label1.Size = new Size(223, 21);
+            label1.TabIndex = 3;
+            label1.Text = "Aquamarine Calculator ©2024";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(12, 5);
+            label3.Name = "label3";
+            label3.Size = new Size(209, 13);
+            label3.TabIndex = 5;
+            label3.Text = "Aquamarine Natural Number Calculator";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkSlateGray;
+            ClientSize = new Size(338, 450);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Aquamarine Calculator";
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private RichTextBox richTextBox3;
+        private GroupBox groupBox2;
+        private GroupBox groupBox4;
+        private Button button1;
+        private GroupBox groupBox3;
+        private RichTextBox richTextBox2;
+        private RichTextBox richTextBox1;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Label label1;
+        private Label label3;
+        private Button button5;
+    }
+}
---
+++ AquamarineCalculator/Form1.cs
@@ -0,0 +1,72 @@
+namespace AquamarineCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(richTextBox1.Text);
+            int num2 = Convert.ToInt32(richTextBox1.Text);
+            int outputI = num1 + num2;
+            string output = outputI.ToString();
+            richTextBox3.Text = output;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(richTextBox1.Text);
+            int num2 = Convert.ToInt32(richTextBox1.Text);
+            int outputI = num1 - num2;
+            string output = outputI.ToString();
+            richTextBox3.Text = output;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(richTextBox1.Text);
+            int num2 = Convert.ToInt32(richTextBox1.Text);
+            int outputI = num1 * num2;
+            string output = outputI.ToString();
+            richTextBox3.Text = output;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(richTextBox1.Text);
+            int num2 = Convert.ToInt32(richTextBox1.Text);
+            int outputI = num1 / num2;
+            string output = outputI.ToString();
+            richTextBox3.Text = output;
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(richTextBox3.Text);
+        }
+    }
+}
